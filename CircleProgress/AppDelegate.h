2deb39d4683f05c16d8203e9ac9faf4b0c5373d7
悦悦 <yue@yueyuedeMacBook-Pro.local>
2018-04-15 12:10:20 +0800

    Initial Commit

---
+++ CircleProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleProgress
+//
+//  Created by 悦悦 on 2018/4/15.
+//  Copyright © 2018年 悦悦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircleProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircleProgress
+//
+//  Created by 悦悦 on 2018/4/15.
+//  Copyright © 2018年 悦悦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
